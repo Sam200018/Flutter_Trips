1cebd4e9cb5d0e8ae13758828d83b640cd1e31c2
Sam Bautista <samuelbaubas@gmail.com>
2020-08-26 17:32:18 -0500

    Se agrego una nueva clase para poder recibir info de alguna API y ya poder mostrarlo

---
+++ lib/description_place.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  //cosntructor
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
+  @override
+  Widget build(BuildContext context) {
+    final star_half = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2c611),
+      ),
+    );
+    final star_empty = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star_border,
+      ),
+    );
+
+    final star = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFf2c611),
+      ),
+    );
+
+    final descrption = Container(
+      margin: EdgeInsets.only(
+        top: 20.0,
+        right: 20.0,
+        left: 20.0,
+      ),
+      child: Text(
+        descriptionPlace,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w300,
+          color: Color(0xFF56575a),
+        ),
+      ),
+    );
+
+    final title_stars = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Text(
+            namePlace,
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            star,
+            star,
+            star,
+            star,
+            star_empty,
+          ],
+        ),
+      ],
+    );
+    return Column(
+      children: <Widget>[
+        title_stars,
+        descrption,
+      ],
+    );
+  }
+}
