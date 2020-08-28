c2610769f99b62a469be7cc9d44117858d593460
Sam Bautista <samuelbaubas@gmail.com>
2020-08-27 23:45:52 -0500

    Widget Apilando textos, haciendo que quedé en columna

--- lib/review.dart
+++ lib/review.dart
@@ -2,9 +2,64 @@ import 'package:flutter/material.dart';
 
 class Review extends StatelessWidget {
   String pathImage = 'assets/img/people.jpg';
-  Review(this.pathImage);
+  String name = 'Varuna Yasas';
+  String details = '1 review 5 photos';
+  String comment = 'There is an amazing place in Sri Lanka';
+  Review(this.pathImage, this.name, this.details, this.comment);
   @override
   Widget build(BuildContext context) {
+    final userComment = Container(
+      margin: EdgeInsets.only(
+        left: 20.0,
+      ),
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 13.0,
+          fontFamily: 'Lato',
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    );
+
+    final userInfo = Container(
+      margin: EdgeInsets.only(
+        left: 20.0,
+      ),
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 13.0,
+          fontFamily: 'Lato',
+          color: Color(0xFFa3a5a7),
+        ),
+      ),
+    );
+
+    final userName = Container(
+      margin: EdgeInsets.only(
+        left: 20.0,
+      ),
+      child: Text(
+        name,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 17.0,
+          fontFamily: 'Lato',
+        ),
+      ),
+    );
+
+    final userDetails = Column(
+      children: <Widget>[
+        userName,
+        userInfo,
+        userComment,
+      ],
+    );
+
     final photo = Container(
       margin: EdgeInsets.only(
         top: 20.0,
@@ -23,6 +78,7 @@ class Review extends StatelessWidget {
     return Row(
       children: <Widget>[
         photo,
+        userComment,
       ],
     );
   }
